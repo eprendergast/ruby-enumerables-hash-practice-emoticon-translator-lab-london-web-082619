b1a0da178f2dbdcdc672132bd91157e15cbd59cd
Elizabeth Prendergast <elizabeth.prendergast@gmail.com>
2019-07-01 20:35:47 +0000

    Automatically backed up by Learn

--- lib/sandbox.rb
+++ lib/sandbox.rb
@@ -21,27 +21,28 @@ def load_library(file_path)
   #set each Japanese emoticon equal to the key in the get_meaning hash
   emoticons.each do |meaning, emoticons|
     emoticons_cleaned["get_meaning"]["#{emoticons[1]}"] = meaning
+    emoticons_cleaned["get_emoticon"]["#{emoticons[0]}"] = emoticons[1]
   end
   puts emoticons_cleaned
 end
 
 load_library(file_path)
-#
-# return = {
-#   "get_meaning" => {
-#     "JAPANESE_EMOTICON_1" => "MEANING_1",
-#     "JAPANESE_EMOTICON_2" => "MEANING_2"
-#   },
-#   "get_emoticon" => {
-#     "ENGLISH_EMOTICON_1" =>  "JAPANESE_EMOTICON_1",
-#     "ENGLISH_EMOTICON_2" =>  "JAPANESE_EMOTICON_2",
-#   }
-# }
-#
-# emoticons = {
-#   "angel"=>["O:)", "☜(⌒▽⌒)☞"],
-#   "angry"=>[">:(", "ヽ(ｏ`皿′ｏ)ﾉ"],
-#   "bored"=>[":O", "(ΘεΘ;)"],
-#   "confused"=>["%)", "(゜.゜)"],
-#   "embarrased"=>[":$", "(#^.^#)"]
-# }
+
+return = {
+  "get_meaning" => {
+    "JAPANESE_EMOTICON_1" => "MEANING_1",
+    "JAPANESE_EMOTICON_2" => "MEANING_2"
+  },
+  "get_emoticon" => {
+    "ENGLISH_EMOTICON_1" =>  "JAPANESE_EMOTICON_1",
+    "ENGLISH_EMOTICON_2" =>  "JAPANESE_EMOTICON_2",
+  }
+}
+
+emoticons = {
+  "angel"=>["O:)", "☜(⌒▽⌒)☞"],
+  "angry"=>[">:(", "ヽ(ｏ`皿′ｏ)ﾉ"],
+  "bored"=>[":O", "(ΘεΘ;)"],
+  "confused"=>["%)", "(゜.゜)"],
+  "embarrased"=>[":$", "(#^.^#)"]
+}
