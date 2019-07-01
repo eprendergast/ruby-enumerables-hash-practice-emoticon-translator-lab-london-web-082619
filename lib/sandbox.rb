19e589f96d299d2d130e3f7aea04c6fbc528ac66
Elizabeth Prendergast <elizabeth.prendergast@gmail.com>
2019-07-01 20:31:52 +0000

    Automatically backed up by Learn

--- lib/sandbox.rb
+++ lib/sandbox.rb
@@ -4,10 +4,38 @@ file_path = "./lib/emoticons.yml"
 def load_library(file_path)
   require "yaml"
   emoticons = YAML.load_file(file_path)
-  puts emoticons
+  #=> 
+      emoticons = {
+        "angel"=>["O:)", "☜(⌒▽⌒)☞"],
+        "angry"=>[">:(", "ヽ(ｏ`皿′ｏ)ﾉ"],
+        "bored"=>[":O", "(ΘεΘ;)"],
+        "confused"=>["%)", "(゜.゜)"],
+        "embarrased"=>[":$", "(#^.^#)"]
+      }
+  #now need to transform the emoticons hash into the right format
+  #initiate cleaned emoticons hash 
+  emoticons_cleaned = {
+    "get_meaning" => {},
+    "get_emoticon" => {}
+  }
+  #set each Japanese emoticon equal to the key in the get_meaning hash
+  emoticons.each do |meaning, emoticons|
+    get_meaning["#{emoticons[1]}"] = meaning
+
+  end
+  
 end
 
-load_library(file_path)
+return = {
+  "get_meaning" => {
+    "JAPANESE_EMOTICON_1" => "MEANING_1",
+    "JAPANESE_EMOTICON_2" => "MEANING_2"
+  },
+  "get_emoticon" => { 
+    "ENGLISH_EMOTICON_1" =>  "JAPANESE_EMOTICON_1",
+    "ENGLISH_EMOTICON_2" =>  "JAPANESE_EMOTICON_2",
+  }
+}
 
 emoticons = {
   "angel"=>["O:)", "☜(⌒▽⌒)☞"],
@@ -15,11 +43,4 @@ emoticons = {
   "bored"=>[":O", "(ΘεΘ;)"],
   "confused"=>["%)", "(゜.゜)"],
   "embarrased"=>[":$", "(#^.^#)"]
-}
-
-return = {
-  "get_meaning" => {
-    "JAPANESE_EMOTICON_1" =>  {},
-    "JAPANESE_EMOTICON_2"
-  "get_emoticon" => { }
 }
