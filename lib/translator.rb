a14e10e5d349910e378426fd97a0074060b75888
Elizabeth Prendergast <elizabeth.prendergast@gmail.com>
2019-07-01 20:38:46 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,14 +2,32 @@
 
 file_path = "./lib/emoticons.yml"
 
-#loads the `emoticons.yml` file
+#loads the `emoticons.yml` file and parses the data to the correct format
 def load_library(file_path)
   require "yaml"
   emoticons = YAML.load_file(file_path)
-  emoticons
+  #output of new emoticons hash =>
+      # emoticons = {
+      #   "angel"=>["O:)", "☜(⌒▽⌒)☞"],
+      #   "angry"=>[">:(", "ヽ(ｏ`皿′ｏ)ﾉ"],
+      #   "bored"=>[":O", "(ΘεΘ;)"],
+      #   "confused"=>["%)", "(゜.゜)"],
+      #   "embarrased"=>[":$", "(#^.^#)"]
+      # }
+  #now need to transform the emoticons hash into the right format
+  #initiate cleaned emoticons hash
+  emoticons_cleaned = {
+    "get_meaning" => {},
+    "get_emoticon" => {}
+  }
+  emoticons.each do |meaning, emoticons|
+      #set each Japanese emoticon equal to the key in the get_meaning hash; set the meaning to the value
+    emoticons_cleaned["get_meaning"]["#{emoticons[1]}"] = meaning
+    #set each English emoticon equal to the key in the get_emoticon has; set the Japanse emoticon to the value
+    emoticons_cleaned["get_emoticon"]["#{emoticons[0]}"] = emoticons[1]
+  end
+  emoticons_cleaned
 end
-
-load_library(file_path)
 
 
 
