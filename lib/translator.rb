a5f903f0275ed686024272d6d2906ab63b2cdfc9
Elizabeth Prendergast <elizabeth.prendergast@gmail.com>
2019-07-01 19:59:34 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,21 @@
 # require modules here
 
-def load_library
-  # code goes here
+file_path = "./lib/emoticons.yml"
+
+#loads the `emoticons.yml` file
+def load_library(file_path)
+  require "yaml"
+  emoticons = YAML.load_file(file_path)
+  emoticons
 end
 
+
+#takes a traditional Western emoticon, like `:)` and translate it to its Japanese version.
 def get_japanese_emoticon
   # code goes here
 end
 
+#takes a Japanese emoticon and returns its meaning in English.
 def get_english_meaning
   # code goes here
 end
